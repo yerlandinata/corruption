6ecbc0c0f9831c9cfb9150ad51a9d6b920cf2fc2
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2020-02-16 16:00:52 +0700

    add corruption_categories to root module

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .corruption import Corruption, CorruptionCategory, save_corruption_data, load_provinces_and_cities
+from .corruption import Corruption, CorruptionCategory, save_corruption_data, load_provinces_and_cities, corruption_categories
